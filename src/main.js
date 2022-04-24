e2b51d529f4fcb91c75833fbad0412325ae44963
Filippo Cavallari <filippo.cavallari99@gmail.com>
2022-04-24 22:30:58 +0200

    Add console log

--- src/main.js
+++ src/main.js
@@ -10,19 +10,30 @@ async function main(){
     let projectsJson = await fs.promises.readFile(settings.projectsPath, settings.projectsEncoding)
     let projects = await parseProjectArray(projectsJson)
     for (const proj of projects) {
+        console.log(`Retrieving last commit for project ${proj.projectName}`)
         let latestCommit = await getLatestCommit(proj)
         let sha = latestCommit.sha
+        console.log(sha !== proj.latestCommitSha)
         if(sha !== proj.latestCommitSha){
+            console.log(`Retrieving repository data for project ${proj.projectName}`)
             await proj.repository.getInformation()
+            console.log(`Cloning  project ${proj.projectName}`)
             await proj.clone()
+            console.log(`Building project ${proj.projectName}`)
             let build = await proj.build(latestCommit)
+            console.log(`Saving build ${build.id} for project ${this.projectName}`)
             await proj.saveBuild(build)
+            console.log(`Generating page for project ${proj.projectName}`)
             await handlebar.generatePage(proj)
+            console.log(`Pushing build ${build.id} for project ${this.projectName}`)
             await proj.commitBuild(build)
         }
     }
+    console.log("Saving projects on projects.json")
     await saveProjectArray(projects, settings.projectsPath, settings.projectsEncoding)
+    console.log("Compiling sass")
     await compileSass()
+    console.log("Pushing updated static files")
     await commitStaticFiles()
 }
 
